dd84a881e54f5589278666dd4cced6c78de1fc03
Gustav Nordgren <gustav_nordgren@outlook.com>
2023-01-03 22:17:16 +0100

    update

--- Assignment_A1_03/Program.cs
+++ Assignment_A1_03/Program.cs
@@ -22,6 +22,9 @@ namespace Assignment_A1_03
             //Register the event
             //Your Code
 
+            service.WeatherForecastAvailable += ReportWeatherForecastAvailable; //Subscribed to the broadcaster
+
+
             Task<Forecast>[] tasks = { null, null, null, null };
             Exception exception = null;
             try
@@ -29,7 +32,7 @@ namespace Assignment_A1_03
                 double latitude = 59.5086798659495;
                 double longitude = 18.2654625932976;
 
-                //Create the two tasks and wait for comletion
+                //Create the two tasks and wait for completion
                 tasks[0] = service.GetForecastAsync(latitude, longitude);
                 tasks[1] = service.GetForecastAsync("Miami");
 
@@ -38,7 +41,7 @@ namespace Assignment_A1_03
                 tasks[2] = service.GetForecastAsync(latitude, longitude);
                 tasks[3] = service.GetForecastAsync("Miami");
 
-                //Wait and confirm we get an event showing cahced data avaialable
+                //Wait and confirm we get an event showing cached data avaialable
                 Task.WaitAll(tasks[2], tasks[3]);
             }
             catch (Exception ex)
@@ -46,17 +49,42 @@ namespace Assignment_A1_03
                 exception = ex;
                 //How to handle an exception
                 //Your Code
+                Console.WriteLine("-----------");
+                Console.WriteLine("City weather service error");
+                Console.WriteLine(ex.Message);
             }
 
             foreach (var task in tasks)
             {
                 //How to deal with successful and fault tasks
                 //Your Code
+                var forecastDayGrp = task.Result.Items.GroupBy(d => d.DateTime.Date, d => d);
+                Console.WriteLine();
+                Console.WriteLine($"Weather forecast for: {task.Result.City}");
+                Console.WriteLine();
+                foreach (var forecastDay in forecastDayGrp)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(forecastDay.Key);
+                    Console.WriteLine();
+                    foreach (var item in forecastDay)
+                    {
+                        Console.WriteLine($" {item.DateTime} - Väder: {item.Description}, Temperatur: {item.Temperature}°C, Vind: {item.WindSpeed} m/s");
+                        Console.WriteLine();
+                    }
+                }
+
+                if (task == null) throw new Exception();
             }
         }
 
 
         //Event handler declaration
         //Your Code
+
+        public static void ReportWeatherForecastAvailable(object sender, string message) //Subscriber taking in the message
+        {
+            Console.WriteLine(message);
+        }
     }
 }
--- Assignment_A1_03/Services/OpenWeatherService.cs
+++ Assignment_A1_03/Services/OpenWeatherService.cs
@@ -15,26 +15,39 @@ namespace Assignment_A1_03.Services
     public class OpenWeatherService
     {
         HttpClient httpClient = new HttpClient();
-        
+
         //Cache declaration
         ConcurrentDictionary<(double, double, string), Forecast> cachedGeoForecasts = new ConcurrentDictionary<(double, double, string), Forecast>();
         ConcurrentDictionary<(string, string), Forecast> cachedCityForecasts = new ConcurrentDictionary<(string, string), Forecast>();
 
+        string CacheKeyTimeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm");
+
         // Your API Key
-        readonly string apiKey = "";
+        readonly string apiKey = "459a4733f190f0bda950abaa2f2d5b25";
 
         //Event declaration
         public event EventHandler<string> WeatherForecastAvailable;
-        protected virtual void OnWeatherForecastAvailable (string message)
+        protected virtual void OnWeatherForecastAvailable(string message)
         {
             WeatherForecastAvailable?.Invoke(this, message);
         }
+
         public async Task<Forecast> GetForecastAsync(string City)
         {
-            //part of cache code here to check if forecast in Cache
+            //part of cache code here to check if forecast is in Cache
             //generate an event that shows forecast was from cache
             //Your code
-            
+
+            Forecast fResponse = null;
+
+            (string, string) cacheKey = (City, CacheKeyTimeStamp);
+
+            if (cachedCityForecasts.ContainsKey(cacheKey))
+            {
+                OnWeatherForecastAvailable($"Cached weather forecast for {City} available");
+                return cachedCityForecasts[cacheKey];
+            }
+
             //https://openweathermap.org/current
             var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
             var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
@@ -45,6 +58,11 @@ namespace Assignment_A1_03.Services
             //generate an event with different message if cached data
             //Your code
 
+            if (cachedCityForecasts != null)
+            {
+                WeatherForecastAvailable?.Invoke(cachedCityForecasts, $"Weather forecast for {City} available");
+            }
+            
             return forecast;
 
         }
@@ -53,6 +71,16 @@ namespace Assignment_A1_03.Services
             //part of cache code here to check if forecast in Cache
             //generate an event that shows forecast was from cache
             //Your code
+
+            string CacheKeyTimeStamp = DateTime.Now.ToString("g");
+
+            (double, double, string) cacheKey = (latitude, longitude, CacheKeyTimeStamp);
+
+            if (cachedGeoForecasts.ContainsKey(cacheKey))
+            {
+                OnWeatherForecastAvailable($"Cached weather forecast for {latitude}, {longitude} available");
+                return cachedGeoForecasts[cacheKey];
+            }
 
             //https://openweathermap.org/current
             var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
@@ -63,6 +91,7 @@ namespace Assignment_A1_03.Services
             //part of event and cache code here
             //generate an event with different message if cached data
             //Your code
+
 
             return forecast;
         }
@@ -76,8 +105,19 @@ namespace Assignment_A1_03.Services
 
             //Convert WeatherApiData to Forecast using Linq.
             //Your code
-            var forecast = new Forecast(); //dummy to compile, replaced by your own code
-            return forecast;
+            return new Forecast
+            {
+                City = wd.city.name,
+                Items = wd.list.Select(x => new ForecastItem
+                {
+                    DateTime = UnixTimeStampToDateTime(x.dt),
+                    Temperature = x.main.temp,
+                    WindSpeed = x.wind.speed,
+                    Description = x.weather.FirstOrDefault().description,
+                    Icon = x.weather.FirstOrDefault().icon
+                }).ToList(),
+
+            };
         }
         private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
         {
@@ -87,3 +127,15 @@ namespace Assignment_A1_03.Services
         }
     }
 }
+
+/*
+Modify OpenWeatherService so a Forecast downloaded from the web-api is cached and
+returned for an identical request made within 1 minute. The message from the fired event
+should differ in case of cached or downloaded forecast.
+
+Hint:
+This can be done elegantly using the thread safe version of Dictionary<>. Why thread safe?
+Tuples are excellent for Keys and to get a string of current date/time without seconds can be
+done by DateTime.Now.ToString("yyyy-MM-dd HH:mm") .
+Modify the caller to make multiple requests to show that cached data is received.
+*/
